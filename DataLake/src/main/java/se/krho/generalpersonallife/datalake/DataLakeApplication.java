0a4443cb98852bbd8d2e30c93b3b5a0921006323
Kristoffer Högberg <krippa.hogberg@gmail.com>
2019-01-31 16:01:10 +0100

    Api exist for datalake, but much overhead as well

--- DataLake/src/main/java/se/krho/generalpersonallife/datalake/DataLakeApplication.java
+++ DataLake/src/main/java/se/krho/generalpersonallife/datalake/DataLakeApplication.java
@@ -2,9 +2,16 @@ package se.krho.generalpersonallife.datalake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DataLakeApplication {
+public class DataLakeApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DataLakeApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataLakeApplication.class, args);
