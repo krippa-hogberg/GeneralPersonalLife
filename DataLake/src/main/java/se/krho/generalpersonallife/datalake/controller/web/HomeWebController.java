ee7ed938596cc2086e6b6feb8a8549f237941d5c
Kristoffer Högberg <krippa.hogberg@gmail.com>
2019-01-31 16:16:34 +0100

    Stripped of overhead web things in datalake

--- DataLake/src/main/java/se/krho/generalpersonallife/datalake/controller/web/HomeWebController.java
+++ DataLake/src/main/java/se/krho/generalpersonallife/datalake/controller/web/HomeWebController.java
@@ -4,11 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+/*
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
-
+*/
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
@@ -20,6 +21,7 @@ public class HomeWebController {
     @Autowired
     private HttpServletRequest request;
 
+    /*
     @RequestMapping("/")
     public RedirectView index(RedirectAttributes attributes) {
         return new RedirectView("/home");
@@ -30,5 +32,5 @@ public class HomeWebController {
 
         return "home";
     }
-
+    */
 }
