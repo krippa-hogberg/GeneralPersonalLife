65b5aeff984db3b0d65d6ab52823cd755d3a5e4e
Kristoffer Högberg <krippa.hogberg@gmail.com>
2019-04-26 14:51:49 +0200

    Update HomeWebController.java

--- WebBackend/src/main/java/se/krho/generalpersonallife/webbackend/controller/web/HomeWebController.java
+++ WebBackend/src/main/java/se/krho/generalpersonallife/webbackend/controller/web/HomeWebController.java
@@ -5,8 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
@@ -24,10 +26,12 @@ public class HomeWebController {
         return new RedirectView("/home");
     }
 
-    @RequestMapping("/home")
-    public String home(Map<String, Object> model) {
-
-        return "home";
+    @GetMapping("/home")
+    public ModelAndView home() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("home");
+        mv.getModel().put("message", "FooBar!");
+        return mv;
     }
 
 
